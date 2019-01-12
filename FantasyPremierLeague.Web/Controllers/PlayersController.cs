d24058498dcc421e10fd66ebdca1939f05574333
JonnyRivers <jonny.w.rivers@gmail.com>
2019-01-12 11:55:51 -0800

    Fixed bug where current price was missing fractional part

--- FantasyPremierLeague.Web/Controllers/PlayersController.cs
+++ FantasyPremierLeague.Web/Controllers/PlayersController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FantasyPremierLeague.Web.Model;
@@ -24,7 +25,7 @@ namespace FantasyPremierLeague.Web.Controllers
                 Id = element.Id,
                 Name = $"{element.FirstName} {element.WebName}",
                 Points = element.TotalPoints,
-                NowCost = element.NowCost / 10,
+                NowCost = (float)Math.Round(element.NowCost * 0.1d, 1),
                 MinutesPlayed = element.Minutes,
                 Goals = element.GoalsScored,
                 Assists = element.Assists,
