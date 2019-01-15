62af7d62a9fd4cbf82f6b6d0d083d53780009805
JonnyRivers <jonny.w.rivers@gmail.com>
2019-01-14 21:44:45 -0800

    Using MatTableDataSource to improve sorting

--- FantasyPremierLeague.Web/ClientApp/src/app/players/players.component.ts
+++ FantasyPremierLeague.Web/ClientApp/src/app/players/players.component.ts
@@ -1,55 +1,6 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { Sort } from '@angular/material';
+import { Component, Inject, ViewChild } from '@angular/core';
+import { MatSort, MatTableDataSource } from '@angular/material';
 import { HttpClient } from '@angular/common/http';
-
-@Component({
-  selector: 'app-players',
-  templateUrl: './players.component.html',
-  styleUrls: ['./players.component.css']
-})
-export class PlayersComponent implements OnInit {
-  public players: Player[];
-  sortedPlayers: Player[];
-
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Player[]>(baseUrl + 'api/players').subscribe(result => {
-      this.players = result;
-      this.sortedPlayers = this.players.slice();
-    }, error => console.error(error));
-  }
-
-  ngOnInit(): void {
-    
-  }
-
-  sortData(sort: Sort) {
-    const data = this.players.slice();
-    if (!sort.active || sort.direction === '') {
-      this.sortedPlayers = data;
-      return;
-    }
-
-    this.sortedPlayers = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
-      switch (sort.active) {
-        case 'name': return compare(a.name, b.name, isAsc);
-        case 'points': return compare(a.points, b.points, isAsc);
-        case 'nowCost': return compare(a.nowCost, b.nowCost, isAsc);
-        case 'minutesPlayed': return compare(a.minutesPlayed, b.minutesPlayed, isAsc);
-        case 'goals': return compare(a.goals, b.goals, isAsc);
-        case 'assists': return compare(a.assists, b.assists, isAsc);
-        case 'conceded': return compare(a.conceded, b.conceded, isAsc);
-        case 'cleanSheets': return compare(a.cleanSheets, b.cleanSheets, isAsc);
-        case 'ictIndex': return compare(a.ictIndex, b.ictIndex, isAsc);
-        default: return 0;
-      }
-    });
-  }
-}
-
-function compare(a: number | string, b: number | string, isAsc: boolean) {
-  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
-}
 
 export interface Player {
   id: number;
@@ -62,4 +13,34 @@ export interface Player {
   conceded: number;
   cleanSheets: number;
   ictIndex: number;
+}
+
+@Component({
+  selector: 'app-players',
+  templateUrl: './players.component.html',
+  styleUrls: ['./players.component.css']
+})
+export class PlayersComponent {
+  public players: Player[];
+  dataSource: MatTableDataSource<Player>;
+  displayedColumns: string[] = [
+    'name',
+    'points',
+    'nowCost',
+    'minutesPlayed',
+    'goals',
+    'assists',
+    'conceded',
+    'cleanSheets',
+    'ictIndex'
+  ];
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Player[]>(baseUrl + 'api/players').subscribe(result => {
+      this.players = result;
+      this.dataSource = new MatTableDataSource(this.players);
+      this.dataSource.sort = this.sort;
+    }, error => console.error(error));
+  }
 }
